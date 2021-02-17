2b0be51c38c4a45ff2ce0c0361e449240ebc7ab5
BaxanAcharya <baxanacharya@gmail.com>
2021-02-18 04:28:29 +0545

    user profile

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route, BrowserRouter as Router} from 'react-router-dom';
 import routes from '../config/routes'
 import Home from '../pages/Home/Home';
+import Profile from '../pages/Profile/Profile';
 interface IProps{
 
 }
@@ -9,7 +10,9 @@ interface IProps{
     return (
         <Router>
             <Switch>
-                <Route path={routes.home} component={Home} />
+            <Route path={routes.profile} exact component={Profile} />
+                <Route path={routes.home} exact component={Home} />
+                
             </Switch>
         </Router>
     )
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserTable from '../../component/UserTable'
+
+interface IProps{
+
+}
+const Profile = (props: IProps) => {
+    return (
+        <div>
+            Profile
+        </div>
+    )
+}
+
+export default Profile
